d77ed601a5b399903c77a55171fa06c81ebab4b4
SUP3RIA <sup3ria@protonmail.com>
2016-10-15 13:58:57 +0000

    Update atlr.py

--- atlr.py
+++ atlr.py
@@ -19,6 +19,20 @@ import gevent.monkey
 socket.setdefaulttimeout(3)
 gevent.monkey.patch_all()
 
+
+print """
+       db              88                                           
+      d88b       ,d    88                         ,d                
+     d8'`8b      88    88                         88                
+    d8'  `8b   MM88MMM 88 ,adPPYYba, 8b,dPPYba, MM88MMM 8b,dPPYba,  
+   d8YaaaaY8b    88    88 ""     `Y8 88P'   `"8a  88    88P'   "Y8  
+  d8""""""""8b   88    88 ,adPPPPP88 88       88  88    88          
+ d8'        `8b  88,   88 88,    ,88 88       88  88,   88          
+d8'          `8b "Y888 88 `"8bbdP"Y8 88       88  "Y888 88          
+      Imap checker v2.0                          by sup3ria
+"""
+
+
 parser = argparse.ArgumentParser(description='Atlantr Imap Checker 1.1')
 parser.add_argument('-i','--input', help="Inputfile", required=False,type=str,default="mail_pass.txt")
 parser.add_argument('-o','--output', help='Outputfile', required=False,type=str,default="mail_pass_valid.txt")
@@ -154,6 +168,7 @@ def sub_worker(task):
     if l == 'OK':
         logger1.debug(task)
         count_valid = count_valid + 1
+        pbar2.update()
     if l == False:
         logger4.debug(task)
         count_invalid = count_invalid  + 1
@@ -186,7 +201,7 @@ def loader():
         if yes == False:
             os.remove("count.log")
             par1 = 0
-    print "Preparing list (this might take a while)."
+    #print "Preparing list (this might take a while)."
     with open(file_in, "r") as text_file:
         for line in itertools.islice(text_file, par1, None):
             if len(line.strip()) > 1 and ':' in line.strip():
@@ -207,14 +222,16 @@ def asynchronous():
     gevent.joinall(threads,raise_error=True)
     end = timer()
     pbar.close()
-    print "\n\nTime passed: " + str(end - start)[:6]
+    pbar2.close()
     print "\nFound",count_valid, "valid Accounts.","\n(",count_invalid,'invalid and for',count_unmatched,'no settings were found.)'
+    print "\nTime passed: " + str(end - start)[:6]
 
 
 init_ImapConfig()
 q = gevent.queue.JoinableQueue()
 gevent.spawn(loader).join()
 pbar = tqdm(total=q.qsize())
+pbar2 = tqdm(total=q.qsize(),bar_format="  Valid:{n_fmt}")
 asynchronous()
 
 try:
